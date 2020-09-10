992096a978cfd89a77ac7ae33debf4a27cf7c080
ElectronHippo <66708980+ElectronHippo@users.noreply.github.com>
2020-09-10 14:44:40 +1200

    Added a name function.

--- PaperScissorsRockLizardSpock.js
+++ PaperScissorsRockLizardSpock.js
@@ -8,7 +8,35 @@ rules = [ //Defines the rules of the game.//
 ];
 playerscore = 0; //Defines the 'score' variable - this is used to store the players score.//
 computerscore = 0; //Defines the 'computerscore' variable - this is used to store the computers score.//
+function init(){
+    while(true){
+        promptMessage = 'Enter your name (Must be less than 20 characters)'
+        name = window.prompt(promptMessage);
+        console.log('reeeee')
+        console.log(name.split('<').length)
+       if(name.split('<').length == 1){
+            if(name.length <= 20 && name.length != 0){ //If the inputted message meets all parameters, then return it.//
+                defineButtons()
+                return name;
+            }
+            else{
+                if(name.length > 20){ //If the length of their name exceeds the maximum of 20 characters, tell them no.//
+                    alert('Name may not be longer than 20 characters');
+                }
+                else if(name.length == 0){ //If the person doesnt bother entering a name, give them one.//
+                    name = 'товарищ сталин';
+                    defineButtons()
+                    return 'товарищ сталин';
+                };
+            };
+        }
+        else{
+        alert('You may not put angled brackets into your name')
+        };
+    };
+};
 function defineButtons(){ //Defines the buttons used to select the Option.//
+    document.getElementById("textArea").innerHTML = name + "'s Score: 0 / 0";
     for(i = 0; i != rules.length; i++){ //While i is less than the total lines in 'rules'.//
         buttonName = rules[i].split(' ')[0]; //Defines the name of the button as the first word in the rule at i.//
         document.getElementById('buttonArea').innerHTML += '<button class="button" onclick="main('+ i +')">' + buttonName +'</button> '; //Adds buttons to the 'buttonArea' span. Each button has a different value that it starts main() with. This corresponds to the location of the word on the button in the 'rules' list//
@@ -22,17 +50,17 @@ function calc(userSelection, computerSelection){ //Calculates the victor.//
     for(i = 0; i <= rules[0].split('&').length; i++){ //While the variable i is less than or equal to the number of '&' symbols - This is to cater for more or less 'rules'.//
         if (rules[userSelection].split(' ')[0] == rules[computerSelection].split('& ')[i].split(' ')[2]){ //if the player's choice equals any of the things that the computer's choice kills.//
             computerscore +=1; // adds one to the computer's score.//
-            return rules[computerSelection].split('& ')[i] + ' | Score: ' + playerscore + ' / ' + computerscore; //If the computer wins, return the relevant death message.//
+            return rules[computerSelection].split('& ')[i] + ' | ' + name + "'s Score: " + playerscore + ' / ' + computerscore; //If the computer wins, return the relevant death message.//
          };
         if (rules[computerSelection].split(' ')[0] == rules[userSelection].split('& ')[i].split(' ')[2]){ //if the computer's choice equals any of the things that the player's choice kills.//
             playerscore +=1; // adds one to the player's score.//
-            return rules[userSelection].split('& ')[i] + ' | Score: ' + playerscore + ' / ' + computerscore; //If the player wins, return the relevant death message.//
+            return rules[userSelection].split('& ')[i] + ' | ' + name + "'s Score: " + playerscore + ' / ' + computerscore; //If the player wins, return the relevant death message.//
          };
-        if (computerSelection == userSelection){
-            return "It's a draw!" + ' | Score: ' + playerscore + ' / ' + computerscore; //If its a draw, return 'its a draw'.//
+        if (computerSelection == userSelection){ //If its a draw.//
+            return "It's a draw!" + ' | ' + name + "'s Score: " + playerscore + ' / ' + computerscore; //If its a draw, return 'its a draw'.//
         };
     };
-}
+};    
 function main(userSelection){ //This is called upon the pressing of a button.
     document.getElementById("textArea").innerHTML = calc(userSelection, computerSelect()); //Sends the output of calc() to the 'textArea' span.//
 };  
